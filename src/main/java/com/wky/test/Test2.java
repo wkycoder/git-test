b66f93e156b2fbe929ca7530aba355ea45c0e5bd
wangkunyang <wangkunyang@ruigushop.com>
2021-07-11 19:45:11 +0800

    feat:rebase commit

---
+++ src/main/java/com/wky/test/Test2.java
@@ -0,0 +1,13 @@
+package com.wky.test;
+
+/**
+ * @author: wangkunyang
+ * @date 2021/07/11 20:39
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("master test2 class");
+    }
+
+}

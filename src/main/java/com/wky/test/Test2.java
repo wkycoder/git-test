df9e32608b190bb493e9190482c086cffe25b963
wangkunyang <wangkunyang@ruigushop.com>
2021-07-11 20:38:58 +0800

    feat:add test2.java

--- src/main/java/com/wky/test/Test2.java
+++ src/main/java/com/wky/test/Test2.java
@@ -2,11 +2,12 @@ package com.wky.test;
 
 /**
  * @author: wangkunyang
- * @date 2021/07/11 20:39
+ * @date 2021/07/11 20:38
  */
 public class Test2 {
 
     public static void main(String[] args) {
+        System.out.println("test2 class");
         System.out.println("master test2 class");
     }
 

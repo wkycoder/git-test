d81356a3b4af73d6aa1f6471bb139539aea2ad22
wangkunyang <wangkunyang@ruigushop.com>
2021-07-11 21:11:17 +0800

    feat:add feature a

--- src/main/java/com/wky/test/Test2.java
+++ src/main/java/com/wky/test/Test2.java
@@ -9,6 +9,8 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("test2 class");
         System.out.println("master test2 class");
+
+        System.out.println("feature a");
     }
 
 }

e7a8cd230d79a96353ef23441e16a4c0ddb0e72e
wangkunyang <wangkunyang@ruigushop.com>
2022-04-27 07:18:44 +0800

    feat:a commit

--- src/main/java/com/wky/test/Test2.java
+++ src/main/java/com/wky/test/Test2.java
@@ -9,7 +9,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("test2 class");
         System.out.println("master test2 class");
-
+        System.out.println("master add a commit");
         System.out.println("feature a");
         System.out.println("feature b");
         System.out.println("feature c");

15e0a5ab6ee392ea76e9a80ee0424c40b9c59486
wangkunyang <wangkunyang@ruigushop.com>
2022-04-27 07:17:37 +0800

    feat:新增功能分支

--- src/main/java/com/wky/test/Test2.java
+++ src/main/java/com/wky/test/Test2.java
@@ -13,6 +13,8 @@ public class Test2 {
         System.out.println("feature a");
         System.out.println("feature b");
         System.out.println("feature c");
+
+        System.out.println("v1.0");
     }
 
 }

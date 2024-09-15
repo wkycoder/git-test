368a55f9fd25fbdc3eb953cbc480d9a677052059
wangkunyang <wkycoder@163.com>
2024-09-15 21:03:26 +0800

    feat:test revert merge commit

---
+++ src/main/java/com/wky/test/TestV2.java
@@ -0,0 +1,12 @@
+package com.wky.test;
+
+/**
+ * @author wuming
+ * @date 2024/9/15/09/15 21:02
+ */
+public class TestV2 {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
